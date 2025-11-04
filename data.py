50980644578371338e7030e3187bda23f2772576
Faisasa12 <faisalmufadhi@gmail.com>
2025-11-04 19:41:18 +0000

    We tokenized the dataset

--- data.py
+++ data.py
@@ -5,3 +5,9 @@ dataset = load_dataset("sst2")
 
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
+def tokenize(record):
+    return tokenizer(record["sentence"], truncation = True, padding = "max_length")
+
+tokenized_dataset = dataset.map(tokenize, batched = True)
+
+print(tokenized_dataset["train"][0])
