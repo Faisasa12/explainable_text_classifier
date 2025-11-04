02c4498a082187270891ef42fa4d1f1e538cac9a
Faisasa12 <faisalmufadhi@gmail.com>
2025-11-04 20:08:16 +0000

    Load model and tokenizer

--- tune_model.py
+++ tune_model.py
@@ -1,6 +1,11 @@
 import pickle
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 with open("data/tokenized_dataset.pkl", "rb") as file:
     tokenized_dataset = pickle.load(file)
     
 print(tokenized_dataset["train"][0])
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased", clean_up_tokenization_spaces = True)
+
+model = AutoModelForSequenceClassification("distilbert-base-uncased", num_labels = 2)
