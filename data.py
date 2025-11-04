bfad448dca588cbcce01609504ab41ab9eb95b8a
Faisasa12 <faisalmufadhi@gmail.com>
2025-11-04 19:34:03 +0000

    Got distilbert tokenizer

--- data.py
+++ data.py
@@ -1,2 +1,7 @@
 from datasets import load_dataset
+from transformers import AutoTokenizer
+
 dataset = load_dataset("sst2") 
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
