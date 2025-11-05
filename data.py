066b893f427f3c51ac4828dad7f55525f06047af
Faisasa12 <faisalmufadhi@gmail.com>
2025-11-05 13:47:19 +0000

    Saves a subset of the data for training

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer
 import pickle
 
 dataset = load_dataset("sst2") 
-
+print(dataset)
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased", clean_up_tokenization_spaces = True)
 
 def tokenize(record):
@@ -13,5 +13,11 @@ tokenized_dataset = dataset.map(tokenize, batched = True)
 
 tokenized_dataset.set_format(type = "torch", columns = ["label", "input_ids", "attention_mask"])
 
-with open("data/tokenized_dataset.pkl", "wb") as file:
-    pickle.dump(tokenized_dataset, file)
+with open("data/tokenized_subaset_train.pkl", "wb") as file:
+    pickle.dump(tokenized_dataset["train"].shuffle().select(range(5000)), file)
+    
+with open("data/tokenized_dataset_val.pkl", "wb") as file:
+    pickle.dump(tokenized_dataset["validation"], file)
+    
+with open("data/tokenized_dataset_test.pkl", 'wb') as file:
+    pickle.dump(tokenized_dataset["test"], file)
